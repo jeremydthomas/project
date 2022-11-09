f9ea009382c8091301f222541cf03ad53a07667d
Jeremy Thomas <tommyhilfiger12@hotmail.com>
2022-11-09 13:10:15 -0600

    finished template

--- tests/HelloWorld.ts
+++ tests/HelloWorld.ts
@@ -1,22 +1,42 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { HelloWorld } from "../typechain-types";
 
 describe("Hello World", async () => {
-  it("Should give a Hello World", async () => {
+  let helloWorldContract: HelloWorld;
+  let signers: SignerWithAddress[];
+
+  beforeEach(async function () {
+    signers = await ethers.getSigners();
     const helloWorldFactory = await ethers.getContractFactory("HelloWorld");
-    const helloWorldContract = await helloWorldFactory.deploy();
+
+    helloWorldContract = (await helloWorldFactory.deploy()) as HelloWorld;
+
     await helloWorldContract.deployed();
+  });
+  it("Should give a Hello World", async () => {
     const text = await helloWorldContract.helloWorld();
     expect(text).to.equal("Hello World");
   });
 
   it("should set owner to deployer", async () => {
-    const signers = await ethers.getSigners();
-    const helloWorldFactory = await ethers.getContractFactory("HelloWorld");
-    const helloWorldContract = await helloWorldFactory.deploy();
-    await helloWorldContract.deployed();
     const owner = await helloWorldContract.owner();
     const contractDeployer = signers[0].address;
     expect(owner).to.equal(contractDeployer);
+  });
+
+  it("Should change text correctly", async () => {
+    const newText = "New Text";
+    const tx = await helloWorldContract.setText(newText);
+    await tx.wait();
+    const text = await helloWorldContract.helloWorld();
+    expect(text).to.equal(newText);
+  });
+
+  it("Should not allow anyone other than owner to change text", async function () {
+    const newText = "New Text";
+    await expect(helloWorldContract.connect(signers[1]).setText(newText)).to.be
+      .reverted;
   });
 });
